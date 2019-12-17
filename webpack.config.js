b9499a455bc1a0a26e6e5ea528b32ec7c3e3fcf2
to5ta <toby4mails@web.de>
2019-12-17 19:01:13 +0100

    modified output of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].online-bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
