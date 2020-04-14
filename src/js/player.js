9e7a621bf2a9676b766611b9fb53cc1957d91558
to5ta <toby4mails@web.de>
2020-04-14 22:21:23 +0200

    player controls (strafe and rotate)

--- src/js/player.js
+++ src/js/player.js
@@ -6,31 +6,27 @@ class Player {
     constructor(scene, canvas, world) {
         this.scene = scene;
         this.canvas = canvas;
-        this.falling = true;
         this.world = world;
-
+        
         // this.camera = new BABYLON.UniversalCamera(
-        //     "Camera",
+            //     "Camera",
         //     new BABYLON.Vector3(5, 5, 0), 
         //     this.scene);
-
+        
         this.camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0,0,0), this.scene);
         this.camera.radius = 10;
         this.camera.heightOffset = 3;
         this.camera.rotationOffset = 0;
         this.camera.maxCameraSpeed = 1;
         // this.camera.position = new BABYLON.Vector3(10, 2, 0);
-        // this.camera.position = new BABYLON.Vector3(10, 2, 0);
+        // this.camera.positdion = new BABYLON.Vector3(10, 2, 0);
         
         
         // this.camera.inputs.removeByType('FollowCamKeyboardInput');
         // this.camera.attachControl(this.canvas, true);
-
         
         this.scene.activeCamera = this.camera;
-        
-        // this.camera.setTarget(BABYLON.Vector3.Zero());
-        
+                
         this.box = BABYLON.MeshBuilder.CreateBox("PlayerSphere", {size: 2}, this.scene);
         this.box.scaling.x = 0.5;
         this.box.scaling.z = 0.5;
@@ -40,14 +36,14 @@ class Player {
         
         this.camera.lockedTarget = this.box;
         
+        this.falling = true;
         this.fallingVel = 0;
         
         this.moveVel = 0;
         this.moveSpeedMax = 0.1;
         this.moveAcc = 0.1;
         this.moveDamp = 0.1;
-
-        // this.box.rotation.y = Math.PI/2;
+        this.strafe = false;
 
         this.contactRay = new BABYLON.Ray(this.box.position,
             new BABYLON.Vector3(0, -1, 0));
@@ -58,32 +54,42 @@ class Player {
         this.box.onCollideObservable.add((others) => {
             this.falling = false;
             console.log("Ground hit!");
-        });
-
-        
+        });        
     }
 
     handleInput(keyEvent) {
         console.log("KeyEvent in Player:", event);
         
-        const keyReleased = keyEvent.type == "keydown";
+        const keyPressed = keyEvent.type == "keydown";
         if (keyEvent.keyCode == 37){
-            this.inputVec.x = keyReleased ? 1 : 0;
+            if (this.strafe) {
+                this.inputVec.x = keyPressed ? 1 : 0;
+            } else {
+                this.box.rotation.y -= 0.05;
+            }
         }  if(keyEvent.keyCode == 39){
-            this.inputVec.x = keyReleased ? -1 : 0;
+            if (this.strafe) {
+                this.inputVec.x = keyPressed ? -1 : 0;
+            } else {
+                this.box.rotation.y += 0.05;
+            }
         }  if(keyEvent.keyCode == 38){
-            this.inputVec.z = keyReleased ? -1 : 0;
+            this.inputVec.z = keyPressed ? -1 : 0;
         }  if(keyEvent.keyCode == 40){
-            this.inputVec.z = keyReleased ? 1 : 0;
-        }
+            this.inputVec.z = keyPressed ? 1 : 0;
+        }  if(keyEvent.keyCode == 32 && !this.falling){
+            this.falling = true;
+            this.fallingVel = 7;  
+        }  if (keyEvent.keyCode == 16) {
+            this.strafe = keyPressed ? true : false;
+            if (keyPressed) {
+                this.inputVec.x = 0;
+            }
+        }      
+    
         if (this.inputVec.length > 0.9) {
             this.inputVec.normalize();
         }  
-    
-        if(keyEvent.keyCode == 32 && !this.falling){
-            this.falling = true;
-            this.fallingVel = 5;  
-        }        
     }
     
     update(dTimeMs) {
@@ -99,6 +105,19 @@ class Player {
 
         // if(this.moveAcc > 0)
 
+        // const localInput = new BABYLON.Vector3(
+        //     Math.sin(this.box.rotation.y),
+        //     0,
+        //     Math.cos(this.box.rotation.y)
+        // );
+
+        const rotation_matrix = new BABYLON.Matrix.RotationYawPitchRoll(
+            this.box.rotation.y,
+            0,
+            0
+        );
+        
+
         if (this.falling) {
             this.fallingVel += (this.world.gravity * dTimeSec);
         } else {
@@ -112,7 +131,9 @@ class Player {
 
         this.box.moveWithCollisions(
             this.fallVec.scale(dTimeSec).add(
-            this.inputVec.scale(3*dTimeSec)));
+                BABYLON.Vector3.TransformCoordinates(
+                    this.inputVec.scale(3*dTimeSec), 
+                    rotation_matrix)));
 
         const pick = this.contactRay.intersectsMeshes(this.world.collision_meshes, false);
         if (this.box && pick.length) {
