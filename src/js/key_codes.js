36e9ad4668a070d8071f4e1b27fe1104f838e650
to5ta <toby4mails@web.de>
2020-12-27 10:30:32 +0100

    updated dependencies

---
+++ src/js/key_codes.js
@@ -0,0 +1,16 @@
+
+var KEYCODE = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+    W : 87,
+    A : 65,
+    W : 83,
+    D : 68,
+    SPACEBAR: 32,
+    SHIFT: 16
+}
+
+
+export { KEYCODE };
