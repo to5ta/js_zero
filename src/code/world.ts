054f85603867b02ec18073ea5d069bc1904c517b
to5ta <toby4mails@web.de>
2025-01-12 23:27:03 +0100

    new level with landscape

--- src/code/world.ts
+++ src/code/world.ts
@@ -8,9 +8,7 @@ import sky_pz from  "../assets/textures/skybox/skybox0000_pz.png";
 import sky_nz from  "../assets/textures/skybox/skybox0000_nz.png";
 import medieval_theme_01 from  '../assets/music/medieval_theme_01.mp3';
 import medieval_theme_02 from  '../assets/music/medieval_theme_02.mp3';
-import test_level_model from '../assets/models/level0.gltf';
-import test_level_model_bin from  '../assets/models/level0.bin';
-import box_model from  '../assets/models/box.gltf';
+import test_level_model from '../assets/models/level1.glb';
 
 import * as BABYLON from "@babylonjs/core";
 import "@babylonjs/loaders";
